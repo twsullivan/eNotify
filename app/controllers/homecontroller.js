ed583b9ba2b0ad6554f240ee7c86ad5abdf7f11a
Tim Sullivan <timothy.w.sullivan@gmail.com>
2020-02-21 23:03:20 -0600

    Setup home controller to query db and pass to view

--- app/controllers/homecontroller.js
+++ app/controllers/homecontroller.js
@@ -2,6 +2,19 @@ var exports = module.exports = {}
 
 exports.home = function(req, res) {
 
-    res.render('home');
+    // Get all models
+    var models = require("../models");
 
+    // Get the user model
+    var User = models.user;
+
+    // select firstname, lastname from user
+    User.findAll({
+        attributes: ['firstname', 'lastname'] 
+      }).then((users) => { 
+        users.every(user => console.log(user.firstname + ' ' + user.lastname));
+ 
+        // Render home view and pass in users
+        res.render('home',users);
+    });
 }
